dc31fa350e13fe0e721a249e9f1894b947f475ff
dyh_a <xinzon32177@163.com>
2023-04-14 17:35:03 +0800

    test: fix types

--- src/puppeteer.constants.ts
+++ src/puppeteer.constants.ts
@@ -1,11 +1,11 @@
-import type { LaunchOptions } from 'puppeteer';
+import type { PuppeteerLaunchOptions } from 'puppeteer';
 
 export const PUPPETEER_INSTANCE_NAME = 'PuppeteerInstanceName';
 export const PUPPETEER_MODULE_OPTIONS = 'PuppeteerModuleOptions';
 
 export const DEFAULT_PUPPETEER_INSTANCE_NAME = 'DefaultPuppeteer';
 
-const args: LaunchOptions['args'] = [
+const args: PuppeteerLaunchOptions['args'] = [
   '--allow-insecure-localhost', // Enables TLS/SSL errors on localhost to be ignored (no interstitial, no blocking of requests).
   '--allow-http-screen-capture', // Allow non-secure origins to use the screen capture API and the desktopCapture extension API.
   '--no-zygote', // https://codereview.chromium.org/2384163002
@@ -15,7 +15,7 @@ if (typeof process.getuid === 'function') {
   args.push('--no-sandbox');
 }
 
-export const DEFAULT_CHROME_LAUNCH_OPTIONS: LaunchOptions = {
+export const DEFAULT_CHROME_LAUNCH_OPTIONS: PuppeteerLaunchOptions = {
   headless: true,
   pipe: process.platform !== 'win32',
   args,
